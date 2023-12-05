3e4e0aed4b918db0a36a215ca5a9d02962828225
Onoorrr <onurcelik0739@gmail.com>
2023-12-05 14:23:58 +0300

    -

---
+++ RetselGames/Areas/Admin/Contollers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetselGames.Web.Areas.Admin.Contollers
+{
+	[Area("Admin")]
+	public class HomeController : Controller
+	{
+		public  IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
+	
--- RetselGames/Program.cs
+++ RetselGames/Program.cs
@@ -14,9 +14,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -26,8 +26,14 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapAreaControllerRoute(name: "Admin", areaName: "Admin", pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
+	endpoints.MapDefaultControllerRoute();
+}
+);
 
 app.Run();
