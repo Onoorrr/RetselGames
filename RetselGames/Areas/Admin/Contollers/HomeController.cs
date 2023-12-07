96d8b702a8519effeea87a06361f04459f7cde84
Onoorrr <onurcelik0739@gmail.com>
2023-12-07 19:09:26 +0300

    -

--- RetselGames/Areas/Admin/Contollers/HomeController.cs
+++ RetselGames/Areas/Admin/Contollers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace RetselGames.Web.Areas.Admin.Contollers
 {
 	[Area("Admin")]
+	[Authorize]
 	public class HomeController : Controller
 	{
 		public  IActionResult Index()
